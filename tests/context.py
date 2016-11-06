a155de14771e6e47b54805e8b98c9625ae11a3c9
hoxo-m <hoxo.smile@gmail.com>
2016-11-06 10:35:35 +0900

    fix

--- tests/context.py
+++ tests/context.py
@@ -4,6 +4,7 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+import scipy
 from scipy.stats import norm, multivariate_normal
 from numpy import linspace
 from densratio import densratio
