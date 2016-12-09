cd1f9871b33fa5452f801ac16dd94bfd9558b24e
hoxo-m <hoxo.smile@gmail.com>
2016-12-09 21:05:07 +0900

    for pypi

--- setup.py
+++ setup.py
@@ -2,22 +2,16 @@
 
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='densratio',
-    version='0.0.3.9000',
+    version='0.1.3',
     description='A Python Package for Density Ratio Estimation',
-    long_description=readme,
+    long_description='A Python Package for Density Ratio Estimation',
     author='Koji Makiyama',
     author_email='hoxo.smile@gmail.com',
     install_requires=['numpy'],
     url='https://github.com/hoxo-m/densratio_py',
-    license=license,
+    license="MIT + file LICENSE",
     packages=find_packages(exclude=('tests', 'docs')),
     test_suite='tests'
 )
