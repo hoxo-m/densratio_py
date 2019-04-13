6cc344994711333190b5488de8df3d54d3cb658c
Ameya Daigavane <ameya.d.98@gmail.com>
2019-04-13 18:51:14 +0530

    pypi description from README.

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='densratio',
-    version='0.1.4',
+    version='0.2',
     description='A Python Package for Density Ratio Estimation',
-    long_description='A Python Package for Density Ratio Estimation',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Koji Makiyama',
     author_email='hoxo.smile@gmail.com',
     install_requires=['numpy'],
