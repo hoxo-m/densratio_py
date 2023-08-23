fe01dda8898ee608b32a461acaafba218d9fd144
Krzysztof Mierzejewski <kmierz@gmail.com>
2023-08-23 19:24:55 +0200

    Always print 'Normalized vector contains…' warnings.

--- densratio/__init__.py
+++ densratio/__init__.py
@@ -5,4 +5,5 @@ from .RuLSIF import set_compute_kernel_target
 
 
 filterwarnings('default', message='\'numba\'', category=ImportWarning, module='densratio')
+filterwarnings('always', message='Normalized vector contains', category=RuntimeWarning, module=r'densratio\.helpers')
 __all__ = ['alpha_normalize', 'densratio', 'set_compute_kernel_target']
