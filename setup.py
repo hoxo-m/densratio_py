8864de5d381ba9fe008722fbea21ea30569492e8
hoxo-m <hoxo.smile@gmail.com>
2026-05-04 08:48:52 +0900

    Prepare 0.4.0 release docs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='densratio',
-    version='0.3.0',
+    version='0.4.0',
     description='A Python Package for Density Ratio Estimation',
     long_description=long_description,
     long_description_content_type='text/markdown',
